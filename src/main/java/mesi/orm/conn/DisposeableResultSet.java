795709d72af19ea54601d62716299aac88242ee7
mesinger <uintptr96@gmail.com>
2019-10-27 13:02:55 +0100

    disposeable sql resultset

---
+++ src/main/java/mesi/orm/conn/DisposeableResultSet.java
@@ -0,0 +1,24 @@
+package mesi.orm.conn;
+
+import lombok.Getter;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DisposeableResultSet implements AutoCloseable {
+
+    @Getter
+    private final ResultSet resultSet;
+    private final Statement statement;
+
+    DisposeableResultSet(ResultSet resultSet, Statement statement) {
+        this.resultSet = resultSet;
+        this.statement = statement;
+    }
+
+    @Override
+    public void close() throws Exception {
+        resultSet.close();
+        statement.close();
+    }
+}
